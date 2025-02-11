9a27c4ac4406a4c30652d19e355e3b4b876a8cec
omarghemrawi <161760147+omarghemrawi@users.noreply.github.com>
2025-02-11 12:46:55 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+
 
 app.use(cors({
   origin: ['https://e-commerce-frontend-ywzc.onrender.com', 'https://e-commerce-frontend-ywzc.onrender.com/']  // Add both versions
